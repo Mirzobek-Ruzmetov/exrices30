c6594d3b73fdf445caf35cd1d3323d040628314c
Mirzobek Ruzmetov <ruzmetovmirzobek10@gmail.com>
2026-05-07 16:09:39 +0500

    Fix form submission and update todo item creation
    Updated event listener to prevent submission for empty input and modified innerHTML to include input value.

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
-// kerakli html teg lariga ulanish 
+// Kerakli HTML tag lariga ulanish
 
-const form = document.querySelector (". content_form");
-const input = document.querySelector (". form_input");
-const addBtn = document.querySelector (". form_btn");
-const todolist = document.querySelector (". todo_list");
+const form = document.querySelector(".content_form");
+const input = document.querySelector(".form_input");
+const addBtn = document.querySelector(".form_btn");
+const todolist = document.querySelector(".todo_list");
 
-form.addEventListener("subit" , function (e) {
-    e.preventDefault(); // qayta yuklanishni o'chiradi 
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // qayta yuklanishni o'chiradi
+  //  Agar input faqat space dan iborat bo'lsa, dastur to'xtashi kerak
 
-    // Agar input faqat space dan iborat bo'lsa , dastur to'xtatishi kerak
-    if(!input.ariaValueMax.trim()) {
-        return;
-    }
-
-      // input.value - inputdagi qiymatni olish
-    // classlist - html elementlariga class qoshish ochirish uchun ishlatiladi 
-    // innerhtml - js ozgaruvchi ichiga html code qoshish
-    const list_item = document.createElement("div");
-    list_item.classList.add("list_item");
-    list_item.innerHTML = `
+  if (!input.value.trim()) {
+    return;
+  }
+  // input.value - inputdagi qiymatni olish
+  // classlist - html elementlariga class qo'shish, o'chirish uchun ishlatiladi
+  // innerHTML - js o'zgaruvchilari ichiga html code qo'shish
+  const list_item = document.createElement("div");
+  list_item.classList.add("list_item");
+  list_item.innerHTML = `
     <div class="list_info">
-                    <input type="checkbox" type="checkbox">
-                    <p class="todo">Lorem, ipsum dolor.</p>
-                </div>
-                <button class="remove_btn">
-                    <img src="img/trash.svg" width="20" height="20" alt="trash-icon">
-                </button>
-                `;
-
-                todolist.appendChild(list_item);
-                input.value = "";
-
-
-}) ;
+        <input class="checkbox" type="checkbox">
+        <p class="todo">${input.value}</p>
+    </div>
+    <button class="remove_btn">
+        <img src="./img/trash.svg" width="20" height="20" alt="trash-icon">
+    </button>
+  `;
+  todolist.appendChild(list_item);
+  input.value = "";
+});
